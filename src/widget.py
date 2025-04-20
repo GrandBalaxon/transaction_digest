f23d7b69a2f441f3d3b69998dc96ec57baf4b810
Vlad Spinul <shinokakaov@mail.ru>
2025-04-20 21:00:47 +0300

    add get_date func

--- src/widget.py
+++ src/widget.py
@@ -36,10 +36,16 @@ def mask_account_card(type_and_number: str) -> str:
     return " ".join(temp_list)
 
 
+def get_date(date: str) -> str:
+    """Функция возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
+
 if __name__ == "__main__":
     print(
         get_mask_card_number(7000792289606361),
         get_mask_account(73654108430135874305),
         mask_account_card("Счет 73654108430135874305"),
+        get_date("2024-03-11T02:26:18.671407"),
         sep="\n",
     )
